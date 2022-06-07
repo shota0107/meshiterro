77e24fdc76062970f6c4a8d8d1afc8d05b98e17c
EC2 Default User <ec2-user@ip-172-31-20-229.ec2.internal>
2022-06-07 11:50:09 +0000

    0607

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     about_path
   end
-  
-  def after_sign_in_path_for(resource)
+
+  def after_sign_out_path_for(resource)
     about_path
   end
 
