6a1f5db467f0fadef98b34f91dad950d3e751998
EC2 Default User <ec2-user@ip-172-31-20-229.ec2.internal>
2022-06-09 08:23:45 +0000

    0609

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get 'homes/about' => 'homes#about', as: "about"
-  resources :users, only: [:show, :edit]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
 
 end
